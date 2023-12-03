7b045692034845cbc4319c1b5415d0974380ea0c
Rosemberg-2023 <152581350+Rosemberg-2023@users.noreply.github.com>
2023-12-03 02:27:52 -0500

    Add files via upload

---
+++ test/vistas/VentasFormTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistas;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jahsi
+ */
+public class VentasFormTest {
+    
+    public VentasFormTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of buscarCliente method, of class VentasForm.
+     */
+    @Test
+    public void testBuscarCliente() {
+        System.out.println("buscarCliente");
+        VentasForm instance = new VentasForm();
+        instance.buscarCliente();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
